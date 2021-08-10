e87f1336380d78dd5afb710ae4f30ec738fdcebd
Maria de los Angeles Rasmussen <m.angeles.rasmussen@gmail.com>
2021-08-10 19:07:47 -0300

    background triangle created

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ import Table from './table.js';
 const AppStyled = styled.main`
   @import url('https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600;700&display=swap');
   background-image: radial-gradient(circle at top, #1f3757 20%, #131537 100%);
-  min-height: 100vh;
-  padding: 2em;
   body{
     font-family: 'Barlow Semi Condensed', sans-serif;
+  }
+  .app-content{
+    min-height: 100vh;
+    padding: 2em;
+    display: flex;
+    box-sizing: border-box;
+    flex-direction: column;
+    justify-content: space-between;
   }
 `
 
@@ -19,8 +25,10 @@ function App() {
   return (
     <AppStyled>
       <Wrapper>
-        <Header/>
-        <Table/>
+        <div className="app-content">
+          <Header/>
+          <Table/>
+        </div>
       </Wrapper>
     </AppStyled>
   );
--- src/token.js
+++ src/token.js
@@ -4,18 +4,20 @@ import styled from 'styled-components';
 const TokenStyled = styled.div`
     background: white;
     width: 130px;
-    height: 130px;
+    height: 125px;
     border: 15px solid ${({color})=> color.base};
     border-radius: 50%;
     box-sizing: border-box;
     box-shadow: 0 5px 0 ${(props)=> props.color.border};
     display: flex;
     cursor: pointer;
+    position: relative;
+    z-index: 2;
     &:active{
         transform: scale(.9);
     }
     .box{
-        box-shadow: 0 -5px 0 #bcc1d5;
+        box-shadow: 0 -4px 0 #babfd4;
         flex: 1;
         align-self: stretch;
         border-radius: 50%;
